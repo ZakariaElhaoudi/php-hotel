121c3c82a1868865cc8cba51714a1595f24f52a0
Zakaria96 <zakarialavoro@gmail.com>
2023-06-20 15:42:12 +0200

    ticket

--- index.php
+++ index.php
@@ -7,55 +7,80 @@
     <title>Document</title>
 </head>
 <body>
-    <?php
-    $hotels = [
+    <div class="container">
+            <table class="table">
+                <thead>
+                    <tr>
+                        <th scope="col">Nome</th>
+                        <th scope="col">Descrizione</th>
+                        <th scope="col">Parcheggio</th>
+                        <th scope="col">Voto</th>
+                        <th scope="col">Distanza dal centro</th>
+                    </tr>
+                </thead>
+                
+                <tbody>
+                    <tr>
+                        <th>
+                        </th>
+                    </tr>
+                
+                    <?php
+                        $hotels = [
 
-        [
-            'name' => 'Hotel Belvedere',
-            'description' => 'Hotel Belvedere Descrizione',
-            'parking' => true,
-            'vote' => 4,
-            'distance_to_center' => 10.4
-        ],
-        [
-            'name' => 'Hotel Futuro',
-            'description' => 'Hotel Futuro Descrizione',
-            'parking' => true,
-            'vote' => 2,
-            'distance_to_center' => 2
-        ],
-        [
-            'name' => 'Hotel Rivamare',
-            'description' => 'Hotel Rivamare Descrizione',
-            'parking' => false,
-            'vote' => 1,
-            'distance_to_center' => 1
-        ],
-        [
-            'name' => 'Hotel Bellavista',
-            'description' => 'Hotel Bellavista Descrizione',
-            'parking' => false,
-            'vote' => 5,
-            'distance_to_center' => 5.5
-        ],
-        [
-            'name' => 'Hotel Milano',
-            'description' => 'Hotel Milano Descrizione',
-            'parking' => true,
-            'vote' => 2,
-            'distance_to_center' => 50
-        ],
+                            [
+                                'name' => 'Hotel Belvedere',
+                                'description' => 'Hotel Belvedere Descrizione',
+                                'parking' => true,
+                                'vote' => 4,
+                                'distance_to_center' => 10.4
+                            ],
+                            [
+                                'name' => 'Hotel Futuro',
+                                'description' => 'Hotel Futuro Descrizione',
+                                'parking' => true,
+                                'vote' => 2,
+                                'distance_to_center' => 2
+                            ],
+                            [
+                                'name' => 'Hotel Rivamare',
+                                'description' => 'Hotel Rivamare Descrizione',
+                                'parking' => false,
+                                'vote' => 1,
+                                'distance_to_center' => 1
+                            ],
+                            [
+                                'name' => 'Hotel Bellavista',
+                                'description' => 'Hotel Bellavista Descrizione',
+                                'parking' => false,
+                                'vote' => 5,
+                                'distance_to_center' => 5.5
+                            ],
+                            [
+                                'name' => 'Hotel Milano',
+                                'description' => 'Hotel Milano Descrizione',
+                                'parking' => true,
+                                'vote' => 2,
+                                'distance_to_center' => 50
+                            ],
 
-    ];
+                        ];
 
 
-    foreach ($hotels as $hotel) {
-        echo "<pre>";
-        var_dump($hotel); 
-        echo "</pre>";
+                        foreach ($hotels as $hotel) {
+                            echo $hotel['name']
+                            echo  "<tr>" 
+                            echo  "<th>  $hotel['name'] </th>"
+                            echo  "</tr>"
 
-    }
-    ?>
+                        }
+                    ?>
+                    
+                </tbody>
+            </table>
+    </div>
+
+    
     
 </body>
 </html>
